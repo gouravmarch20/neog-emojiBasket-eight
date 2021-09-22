bb6ffa4f2eea38ef2f161588b6cf391503da5025
Gourav Mishra <88934396+gouravmarch20@users.noreply.github.com>
2021-09-22 10:00:33 +0530

    add : css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import './App.css'
 const App = () => {
   const [emojiDictionary, setEmojiDictionary] = useState({
     '😄': 'simle',
@@ -6,16 +7,21 @@ const App = () => {
     '😲': 'astonished',
     '😂': 'Tears of Joy '
   })
+
   const [emojiMeaning, setEmojiMeaning] = useState('')
   var faceEmoji = Object.keys(emojiDictionary)
+
   const handleChange = e => {
     let userInput = e.target.value
+    console.log(userInput)
+    console.log(emojiDictionary[userInput])
+
     if (userInput !== '') {
       let meaning = emojiDictionary[userInput]
       if (meaning === undefined) {
-        console.log('Emoji not found in our data emojiDictionary ')
+        setEmojiMeaning('Emoji not found')
       }
-      setEmojiMeaning(userInput)
+      setEmojiMeaning(meaning)
     } else {
       setEmojiMeaning('Output will shown here : ')
     }
@@ -26,30 +32,32 @@ const App = () => {
   }
 
   return (
-    <div>
-      <h1>Emoji Basket </h1>
+    <div className='container'>
+      <h1 className='heading-one'>Emoji Basket </h1>
       <input
         type='text'
         onChange={handleChange}
         placeholder='Enter emoji or click on any emoji'
       />
-      <h1>
-        Name this emoji <span>{emojiMeaning} hii</span>
-        {/* {console.log(emojiMeaning)} */}
-      </h1>
-
-      {faceEmoji.map(emoji => {
-        return (
-          <p
-            key={emoji}
-            onClick={() => {
-              emojiClickHandler(emoji)
-            }}
-          >
-            {emoji}{' '}
-          </p>
-        )
-      })}
+      <h1>Name of this emoji is</h1>
+      <h3 className="heading-three"> {emojiMeaning} </h3>
+      <div className='emoji-data'>
+        <ul className='list-non-bullet'>
+          {faceEmoji.map(emoji => {
+            return (
+              <li
+                className='list-item-inline'
+                key={emoji}
+                onClick={() => {
+                  emojiClickHandler(emoji)
+                }}
+              >
+                {emoji}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
     </div>
   )
 }
